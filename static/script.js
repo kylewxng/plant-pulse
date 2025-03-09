042835f2098234611d9b5ebc28ee4ad67a4bb178
kylewxng <kylewxng06@gmail.com>
2025-03-09 01:10:20 -0800

    added progress bar

--- static/script.js
+++ static/script.js
@@ -43,14 +43,17 @@
 
             // Set the inner HTML for the article
             newArticle.innerHTML = `
-                <input class="articleInput" type="radio" name="articles" checked>
+                <input class="articleInput" type="radio" name="articles" id="article${articleCount}" checked>
                 <label for="article${articleCount}">
                     <h2 class="crop">Tobacco</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width: 75%;"></div> 
+                    </div>
                 </label>
                 <div class="accordion-content">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Tabak_9290019.JPG/800px-Tabak_9290019.JPG" 
                          alt="Tobacco Image" class="accordion-image">
-                    <p></p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                 </div>
             `;
 
